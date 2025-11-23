6278ce2b12b22853efab47d69b0ff4ab4b52ec9d
SaraHAshimzadeh <sara.hashimzade@gmail.com>
2025-11-23 16:49:19 +0300

    last commit

--- script.js
+++ script.js
@@ -2,8 +2,23 @@ let search = document.querySelector('.search-box');
 
 document.querySelector('#search-icon').onclick = () => {
     search.classList.toggle('active');
+    navbar.classList.remove('active');
 
 }
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-icon').onclick = () => {
+    search.classList.remove('active');
+    navbar.classList.toggle('active');
+}
+
+
+window.onscroll = () => {
+    navbar.classList.remove('active');
+    search.classList.remove('active');
+}
+
 
 
 let header = document.querySelector('header');
